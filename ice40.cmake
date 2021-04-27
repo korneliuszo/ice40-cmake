8f104e19138e4b0a2efd1b12cc42dcc2e17ed499
Korneliusz Osmenda <korneliuszo@gmail.com>
2021-04-27 10:12:43 +0200

    Add simulate target

--- ice40.cmake
+++ ice40.cmake
@@ -69,5 +69,49 @@ function(ice40_synth)
 			${SYNTH_VERILOG_DEPENDS}
 		)
 
+endfunction()
+
+function(ice40_sim)
+
+	set(options)
+
+	set(oneValueArgs
+		TARGET
+		TOP_LEVEL_VERILOG
+		YOSYS_PATH
+		NEXTPNR_PATH
+		ICEPACK_PATH
+	)
+	set(multiValueArgs VERILOG_DEPENDS)
+	cmake_parse_arguments(SYNTH "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN})
+
+	find_program(YOSYS_COMMAND yosys
+		HINT ${SYNTH_YOSYS_PATH} ENV YOSYS_PATH
+	)
+
+	find_program(YOSYS_CONFIG_COMMAND yosys-config
+		HINT ${SYNTH_YOSYS_PATH} ENV YOSYS_PATH
+	)
+
+	get_filename_component(TOP_LEVEL_NAME ${SYNTH_TOP_LEVEL_VERILOG} NAME_WE)
+
+	add_custom_target(${SYNTH_TARGET}.hpp
+		COMMAND
+		${YOSYS_COMMAND} -ql ${CMAKE_CURRENT_BINARY_DIR}/${SYNTH_TARGET}-yosys.log -p
+				'read_verilog ${SYNTH_TOP_LEVEL_VERILOG}\; write_cxxrtl ${CMAKE_CURRENT_BINARY_DIR}/${SYNTH_TARGET}.hpp'
+		WORKING_DIRECTORY
+			${CMAKE_CURRENT_SOURCE_DIR}
+		BYPRODUCTS
+			${SYNTH_TARGET}-yosys.log
+		DEPENDS
+			${SYNTH_TOP_LEVEL_VERILOG}
+			${SYNTH_VERILOG_DEPENDS}
+		)
+
+	add_library(${SYNTH_TARGET} INTERFACE)
+	add_dependencies(${SYNTH_TARGET} ${SYNTH_TARGET}.hpp)
+	target_include_directories(${SYNTH_TARGET} INTERFACE ${CMAKE_CURRENT_BINARY_DIR})
+	execute_process(COMMAND ${YOSYS_CONFIG_COMMAND} --datdir OUTPUT_VARIABLE YOSYS_DATADIR OUTPUT_STRIP_TRAILING_WHITESPACE)
+	target_include_directories(${SYNTH_TARGET} INTERFACE ${YOSYS_DATADIR}/include)
 
 endfunction()
